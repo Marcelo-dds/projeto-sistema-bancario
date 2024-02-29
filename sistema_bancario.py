bf4496583c1bbbc00d6e6c5ca211ae8baf0c1e58
marcelo souza <marceloddsweb@gmail.com>
2024-02-29 19:00:45 -0300

    feito a opção saque

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -22,13 +22,37 @@ while True:
 
         if valor > 0:
             saldo += valor 
-            extrato += f"Depósito: {valor:2f}\n" 
+            extrato += f"Depósito: {valor:.2f}\n" 
 
         else:
             print("Operação rejeitada! O valor informado é invalido! ")
     
     elif opcao == "s":
-        print("Saque")
+        valor = float(input("Informe o valor de saque:"))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite 
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+
+            print("Operação falhou! Você não tem saldo suficiente." )
+
+        elif excedeu_limite:
+
+            print("Operação falhou! o valor do saque execede o limite.")
+
+        elif excedeu_saques:
+
+            print("Operação falhou! Número máximo de saques execedeu o limite. ")
+        
+        elif valor > 0 :
+            saldo -= valor
+            extrato += f'Saque: R${valor:.2f}\n'
+            numero_saques += 1
+            
     elif opcao == "e":
         print("Extrato")
         break
